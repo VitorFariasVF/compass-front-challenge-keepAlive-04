54765f4dd84c526afe16feebd0b1a04ad963c73c
VitorFariasVF <joaovitor1081@gmail.com>
2022-10-15 15:16:47 -0300

    data hora js

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,36 @@
+function mostraDia() {
+    var valorDia = new Date();
+    var numeroDia = valorDia.getDay();
+    var diaLista = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+    var dia = diaLista[numeroDia]
+    document.getElementById("dia-cabecalho").innerHTML = (dia)
+    console.log(numeroDia, dia); // 1
+}
+
+function mostraHora() {
+    setInterval(function() {
+    const horafull = new Date()
+    var hora = horafull.getHours()
+    var min = horafull.getMinutes()
+    var seg = horafull.getSeconds()
+    document.getElementById("hora-cabecalho").innerHTML = (hora + ":" + min +":" + seg)
+    }, 1000)
+    
+}
+
+function mostraData() { 
+    var dia = new Date().getDate()
+    var ano = new Date().getFullYear()
+    var mesAno = new Date().getMonth()
+    var mesLista = ["Janeiro", "Fevereiro", "Março", "Abril",   "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    var mes = mesLista[mesAno]
+    document.getElementById("data-cabecalho").innerHTML = (dia + " " + mes + " " + ano)
+}
+
+mostraDia()
+mostraHora()
+mostraData()
+
 var timer = 601
 
 function mostraContador() {
